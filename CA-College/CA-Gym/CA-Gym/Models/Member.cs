6cfdf7a02218a324b40244f01cd1de54d8ecda1e
Martin McCarthy <mccartm6@gmail.com>
2017-08-31 16:12:05 +0100

    added the exercise page

---
+++ CA-College/CA-Gym/CA-Gym/Models/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CA_Gym.Models
+{
+    public class Member
+    {
+        public int memTypeID { get; set; }
+        public string email { get; set; }
+        public string memPass { get; set; }
+        public string firstName { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
